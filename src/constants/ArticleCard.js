ffe77dbd43327b237f01499a4c493d4bcada119b
Niclas Hammar <nicke_hammar@hotmail.com>
2018-03-27 10:36:13 +0200

    Fixed :hover bug

--- src/constants/ArticleCard.js
+++ src/constants/ArticleCard.js
@@ -8,27 +8,27 @@ import '../resources/styles/ArticleCard.css'
 const ArticleCard = (props) => {
 	
   return (
-
-      <Card className='card-article' onClick={props.onClick}>
-        <CardImg className='articlecard-img' top width="100%" src={props.props.main_img_url + props.props._id * 10} alt="Card image cap" />
-        <div className='card-articleinfo'>
-			<div className='card-author'>
-				<img src={userlogo} className='card-avatar' />
-				<p className='skribent'>{props.props.author}</p>
-			</div>
-			<div className='card-timestamp'>
-				<p className=''>{props.props.timestamp}</p>
-			</div>
-		</div>
-        <CardBody>
-          <CardTitle >{props.props.preamble}
-			</CardTitle>
-          <CardText>{props.props.main_content}
-			</CardText>
-         <hr/>
-        </CardBody>
-      </Card>
-      
+      <div className='wrapper-card'>
+        <Card className='card-article' onClick={props.onClick}>
+          <CardImg className='articlecard-img' top width="100%" src={props.props.main_img_url + props.props._id * 10} alt="Card image cap" />
+          <div className='card-articleinfo'>
+  			<div className='card-author'>
+  				<img src={userlogo} className='card-avatar' />
+  				<p className='skribent'>{props.props.author}</p>
+  			</div>
+  			<div className='card-timestamp'>
+  				<p className=''>{props.props.timestamp}</p>
+  			</div>
+  		</div>
+          <CardBody>
+            <CardTitle >{props.props.preamble}
+  			</CardTitle>
+            <CardText>{props.props.main_content}
+  			</CardText>
+           <hr/>
+          </CardBody>
+        </Card>
+      </div>
     
       )
 }
